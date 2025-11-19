fa0f8dbfb1964e9c8150f45053184a74471ff8f4
Hugo Leal <Hugo.souzaleal1@gmail.com>
2025-11-18 23:39:32 -0300

    Atualizando o HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/home/home'
 
 import { DadosProvider } from './hook/useDados'
 
 function App() {
-
   return (
     <DadosProvider>
-
       <Router>
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path="*" element={<h1>Página não encontrada</h1>} />
         </Routes>
       </Router>
-
     </DadosProvider>
   )
 }
